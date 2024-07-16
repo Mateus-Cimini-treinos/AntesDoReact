9dd24ea523ffce87b4453e4214fe2ee948b860f5
Mateus Cimini Araujo <mateusmateus122333@gmail.com>
2024-07-16 19:58:37 -0300

    add 4 topico

---
+++ (4) Template Literals/main.js
@@ -0,0 +1,15 @@
+// (1) Template Literals
+// Os Template Literals são uma ferramenta poderosa no JavaScript moderno, oferecendo uma sintaxe clara e flexível para trabalhar com strings. Eles facilitam a interpolação de variáveis, a criação de strings multilinha e permitem a construção dinâmica de conteúdos de forma eficiente e legível.
+
+
+//definindo variaveis
+
+const nome = "Mateus"
+const idade = 18
+
+document.body.innerHTML = `Meu nome e: ${nome} <br>  E tenho: ${idade} anos`
+
+
+// AREA DE TESTE E APRENDIZADO SOBRE OBJETOS
+//quaso queira testar topico por topico precisa apenas retirar os "//" do topico e dar um refresh na pagina
+
